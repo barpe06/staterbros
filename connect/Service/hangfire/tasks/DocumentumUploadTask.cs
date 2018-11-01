4cb7a66f9872d2a89e8f2bc2aa9ec4a377848251
Pedro Barroso <pedro.barroso@docusign.com>
2018-11-01 12:35:55 -0700

    First commit

---
+++ connect/Service/hangfire/tasks/DocumentumUploadTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DocuSign.Connect;
+using connect.Service.docusign;
+using DocuSign.eSign.Model;
+using connect.Service.docusign.utils;
+using System.ComponentModel;
+using System.IO;
+using connect.Models.documentum;
+using connect.Service.documentum;
+using log4net;
+using connect.Service.documentum.utils;
+
+namespace connect.Service.hangfire.tasks
+{
+    public static class DocumentumUploadTask
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(DocumentumUploadTask));
+
+        //[DisplayName("Send order #{envelopeInfo.EnvelopeStatus.EnvelopeID} to warehouse")]
+        /*public static void uploadDocuments(DocuSignEnvelopeInformation envelopeInfo)
+        {
+            Predicate<CustomField> accountFinder = (CustomField p) => { return p.Name == "AccountId"; };
+            DocuSignService.GetDocument(ServiceUtil.buildEnvironment(envelopeInfo),
+                envelopeInfo.EnvelopeStatus.EnvelopeID,
+                envelopeInfo.EnvelopeStatus.DocumentStatuses.DocumentStatus.ID,
+                DocumentOptions.Individual);
+        }*/
+        [DisplayName("Uploading document {3} for envelope {2}")]
+        public static void uploadDocument(IDictionary<string, string> localParams,
+            string envelopeId, string documentId, DocumentOptions options)
+        {
+            //Now retrieve the documents for the given envelope from the accountId hosted in environment as combined
+            MemoryStream docStream = DocuSignService.GetDocument(localParams[EnvelopeMetaFields.Environment], 
+                localParams[EnvelopeMetaFields.AccountId],
+                envelopeId,
+                documentId,
+                options);
+
+
+            // Now upload the bytes for teh document that we just retrieved in Documentum
+            byte[] buffer = ServiceUtil.ReadFully(docStream);
+            var response = DocumentumService.uploadDocument(localParams, buffer);
+            Log.Debug("Properties created: ");
+        }
+    }
+}   
---
+++ connect/Service/ibm/cm/CMWebServiceClient.cs
@@ -0,0 +1,47 @@
+﻿using connect.CMService;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace connect.Service.ibm.cm
+{
+    public class CMWebServiceClient
+    {
+        //<summary> Sets up an array of MTOMAttachments. 
+        //The MTOMAttachments are used to send the content
+        // of the documents parts to the server in the soap message
+        //</summary>
+        //<param name="resources"> array containing information 
+        //about the document part to be associated 
+        //with the policy </param> 
+        //<returns> new array of MTOMAttachments</returns>
+
+        MTOMAttachment[] setupAttachments(string[,] resources)
+        {   //Create MTOMAttachment objects for all the resources 
+            //passed in the resources array provides
+            // the information for the document parts to be 
+            //sent to the server
+            MTOMAttachment[] attachments = new
+            MTOMAttachment[resources.GetLength(0)];
+            for (int i = 0; i < resources.GetLength(0); i++)
+            {  // resources[i,0] is the ID of the attachment. 
+                //This should match the label of the document part
+                // on the server side, the ID of the attachment and the 
+                //label of the document part are used to  
+                // match the attachment with its associated document part 
+                // resources[i,1] is the mime type of the document part
+                // resources[i,2] is the file path to the document part
+
+                attachments[i] = new MTOMAttachment();
+                attachments[i].ID = resources[i, 0];
+                attachments[i].MimeType = resources[i, 1];
+                attachments[i].Value = File.ReadAllBytes(resources[i, 2]);
+            }
+            return attachments;
+        }
+
+
+    }
+}
